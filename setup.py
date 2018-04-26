57ed97c7a3a16650e577f88999c1d9a73e0d5589
rotsee <leo_wallentin@hotmail.com>
2018-04-26 10:59:58 +0200

    check in

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+from version import version, name, authors, email, short_desc
+
+
+def readme():
+    """Import README for use as long_description."""
+    with open("README.rst") as f:
+        return f.read()
+
+
+setup(
+    name=name,
+    version=version,
+    description=short_desc,
+    long_description=readme(),
+    url="https://github.com/jplusplus/newsworthy-charts",
+    author=authors,
+    author_email=email,
+    license="MIT",
+    packages=[name],
+    zip_safe=False,
+    install_requires=[],
+    include_package_data=True,
+    download_url="https://github.com/jplusplus/newsworthy-charts/archive/%s.tar.gz"
+                 % version,
+)
---
+++ version.py
@@ -0,0 +1,17 @@
+# encoding: utf-8
+from datetime import date
+
+name = "newsworthy-charts"
+
+short_version = "0.0.0"
+long_version = short_version + "-dev0"
+
+short_desc = """\
+Create charts and publish on Amazon S3.\
+"""
+authors = u"Jens Finnäs and Leo Wallentin, J++ Stockholm"
+year = date.today().year
+copyright = "%s, %s" % (year, authors)
+email = "stockholm@jplusplus.org"
+
+version = long_version
