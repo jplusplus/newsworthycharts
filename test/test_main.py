8d6f497ecfed12f09e747ee114ffda811cdc5b5e
rotsee <leo_wallentin@hotmail.com>
2018-05-23 12:15:40 +0200

    test image dimensions

--- test/test_main.py
+++ test/test_main.py
@@ -4,14 +4,25 @@ from newsworthycharts import Chart
 from newsworthycharts import LocalStorage, S3Storage
 from newsworthycharts.storage import DictStorage
 from imghdr import what
+from PIL import Image
 
 
 def test_generating_png():
-
     container = {}
     ds = DictStorage(container)
-    c = Chart(600, 800, storage=ds)
+    c = Chart(800, 600, storage=ds)
     c.render("test", "png")
 
     assert("png" in container)
     assert(what(container["png"]) == "png")
+
+
+def test_file_size():
+    container = {}
+    ds = DictStorage(container)
+    c = Chart(613, 409, storage=ds)
+    c.render("test", "png")
+
+    im = Image.open(container["png"])
+    print(im.size)
+    assert(im.size == (613, 409))
