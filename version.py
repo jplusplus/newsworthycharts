a3873629111d9e40451de746946540d47e08981c
rotsee <leo_wallentin@hotmail.com>
2018-05-27 19:32:33 +0200

    start working on dev11

--- version.py
+++ version.py
@@ -4,7 +4,7 @@ from datetime import date
 name = "newsworthycharts"
 
 short_version = "1.0.0"
-long_version = short_version + ".dev10"
+long_version = short_version + ".dev11"
 
 short_desc = """\
 Create charts and publish on Amazon S3.\
