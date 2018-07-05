acf4288d3518dca11f5763f89a3742c9a34ac844
rotsee <leo_wallentin@hotmail.com>
2018-07-05 23:30:33 +0200

    rc1, yay!

--- version.py
+++ version.py
@@ -4,7 +4,7 @@ from datetime import date
 name = "newsworthycharts"
 
 short_version = "1.0.0"
-long_version = short_version + ".dev20"
+long_version = short_version + "-rc1"
 
 short_desc = """\
 Create charts and publish on Amazon S3.\
