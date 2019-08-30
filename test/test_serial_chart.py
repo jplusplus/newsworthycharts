406d820ceb498754038a76a44ba5ca89b4ccfe83
rotsee <mejl@leowallentin.se>
2019-08-30 11:21:38 +0200

    make test pass

--- test/test_serial_chart.py
+++ test/test_serial_chart.py
@@ -1,24 +1,28 @@
 from newsworthycharts import SerialChart
+from newsworthycharts.storage import DictStorage
+
 
 def test_color_function():
-    c = SerialChart(800, 500)
+    container = {}
+    ds = DictStorage(container)
+    c = SerialChart(800, 500, storage=ds)
     data = [
-      ["2016-01-01",-4],
-      ["2017-01-01",4],
-      ["2018-01-01",None],
-      ["2019-01-01",-1]
+      ["2016-01-01", -4],
+      ["2017-01-01", 4],
+      ["2018-01-01", None],
+      ["2019-01-01", -1]
     ]
     c.color_fn = "positive_negative"
     c.data.append(data)
-    c.type="bars"
+    c.type = "bars"
     # highlighting a bar should not affect this color rule
     c.highlight = "2019-01-01"
 
-    #c.render("test", "png")
+    c.render("test", "png")
 
-    neutral_color = c.style["neutral_color"]
-    pos_color = c.style["positive_color"]
-    neg_color = c.style["negative_color"]
+    neutral_color = c._style["neutral_color"]
+    pos_color = c._style["positive_color"]
+    neg_color = c._style["negative_color"]
     bar_colors = [bar.get_facecolor() for bar in c.ax.patches]
     assert(bar_colors[0] == neg_color)
     assert(bar_colors[1] == pos_color)
