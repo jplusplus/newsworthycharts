96ff56bc95eba36dd2e4bc0d2790f3ce5bf1fe8a
jensfinnas <jens.finnas@gmail.com>
2019-09-03 15:26:43 +0200

    alter colors

--- newsworthycharts/lib/colors.py
+++ newsworthycharts/lib/colors.py
@@ -1,8 +1,8 @@
 # Default colors
 STRONG = "5aa69d"
 NEUTRAL = "999999"
-POSITIVE = "1a9641"
-NEGATIVE = "d7191c"
+POSITIVE = "47b358"
+NEGATIVE = "ec6b56"
 FILL_BETWEEN = "F7F4F4"
 WARM = "ff808f"
 COLD = "4062bb"
