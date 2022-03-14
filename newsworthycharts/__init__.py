3c376b34282282fc41b508714ccc0395500328df
rotsee <mejl@leowallentin.se>
2022-03-14 17:48:59 +0100

    add seasonal chart; dont import with *

--- newsworthycharts/__init__.py
+++ newsworthycharts/__init__.py
@@ -1,15 +1,17 @@
 from .chart import Chart
 from .serialchart import SerialChart
+from .seasonalchart import SeasonalChart
 from .categoricalchart import CategoricalChart, CategoricalChartWithReference, ProgressChart
 from .scatterplot import ScatterPlot
 from .datawrapper import DatawrapperChart
 from .rangeplot import RangePlot
-from .custom.climate_cars import *
+from .custom.climate_cars import ClimateCarsYearlyEmissionsTo2030, ClimateCarsCO2BugdetChart
 from .storage import *
 
 CHART_ENGINES = {
     "Chart": Chart,
     "SerialChart": SerialChart,
+    "SeasonalChart": SeasonalChart,
     "CategoricalChart": CategoricalChart,
     "CategoricalChartWithReference": CategoricalChartWithReference,
     "ProgressChart": ProgressChart,
