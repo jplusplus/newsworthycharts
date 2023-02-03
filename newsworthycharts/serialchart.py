ad8e21b22d0b68dfbe266232cd28f54a70171cd9
rotsee <leo_wallentin@hotmail.com>
2023-02-03 09:30:21 +0100

    fix broken ymin case

--- newsworthycharts/serialchart.py
+++ newsworthycharts/serialchart.py
@@ -472,13 +472,17 @@ class SerialChart(Chart):
             # all values are positive, but also show negatives
             ymin = min(self.ymin, self.data.min_val)
             padding_bottom = abs(ymin * 0.15)
-        elif self.allow_broken_y_axis:
+        elif self.data.min_val > 0 and self.allow_broken_y_axis:
+            # Boken y axis?
             if (self.data.max_val - self.data.min_val) < self.data.min_val:
                 # Only break y axis if data variation is less than distance from ymin to 0
                 ymin = self.data.min_val
                 padding_bottom = abs(self.data.min_val * 0.15)
             else:
                 ymin = 0
+        elif self.data.min_val < 0:
+            ymin = self.data.min_val
+            padding_bottom = abs(-ymin * 0.15)
         else:
             ymin = 0
 
