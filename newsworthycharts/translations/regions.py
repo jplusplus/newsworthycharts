9943210419c537a365287050fbf531b87db20b45
rotsee <leo_wallentin@hotmail.com>
2023-03-20 21:53:00 +0100

    lint

--- newsworthycharts/translations/regions.py
+++ newsworthycharts/translations/regions.py
@@ -4,5 +4,6 @@ HERE = os.path.dirname(__file__)
 
 # Translate Newsworthy region codes
 with open(os.path.join(HERE, "se_municipalities.csv")) as f:
-    NW_MUNI_TO_CLDR = { x["nw_id"]: x["cldr"] 
-                       for x in csv.DictReader(f) }
+    NW_MUNI_TO_CLDR = {
+        x["nw_id"]: x["cldr"] for x in csv.DictReader(f)
+    }
